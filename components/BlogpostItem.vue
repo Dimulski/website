700695a626fcad637e0d2d590cb7d22998183e68
George Dimulski <george.dimulski@gmail.com>
2021-01-12 17:25:52 +0200

    Change BlogpostItem date to coming soon

--- components/BlogpostItem.vue
+++ components/BlogpostItem.vue
@@ -3,7 +3,7 @@
     <div style="" class="block max-w-2xl px-5 md:px-9 py-6 mx-auto bg-white rounded-lg shadow-md cursor-pointer">
       <a class="blog block">
         <div class="flex justify-between items-center">
-          <span class="text-sm text-blue-700 font-light">Coming {{ formattedDate }}</span>
+          <span class="text-sm text-blue-700 font-light">Coming soon</span>
           <span class="text-sm text-black opacity-50 font-light">{{ post.read_time }} minutes</span>
         </div>
         <div class="mt-2 mb-1 opacity-50">

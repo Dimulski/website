b0f84a5ab9782f72e3e79f34688f72751681031d
Georgi Dimulski <george.dimulski@gmail.com>
2022-01-17 23:05:09 +0200

    [next-rebuild] Vercel deploy test

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Quack!
         </h1>
 
         <p className={styles.description}>
